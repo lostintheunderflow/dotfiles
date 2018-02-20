17a49ef2a1ebbb2cb11526ecf92c7b45332d78a2
lost underflow <lostintheunderflow@gmail.com>
2018-02-20 18:39:55 +0000

    i3blocks added time to calendar events

--- bin/.next_cal_out.sh
+++ bin/.next_cal_out.sh
@@ -1,6 +1,6 @@
 source ~/.CAL_LINE
 NUM_LINE=`khal list --format "{title}" | wc -l`
-khal list --format "{title}" | sed -n ''"$CAL_LINE"'p'
+khal list --format "{title} {start-time}" | sed -n ''"$CAL_LINE"'p'
 ((CAL_LINE=CAL_LINE%NUM_LINE))
 ((CAL_LINE++))
 #echo $CAL_LINE $NUM_LINE
