605170695251bd49b1eeb9647002d32ac1097bce
lost underflow <lostintheunderflow@gmail.com>
2018-02-20 14:58:45 +0000

    Calendar khal vdirsyncer showing meetings on i3blocks

---
+++ bin/.next_cal_out.sh
@@ -0,0 +1,10 @@
+source ~/.CAL_LINE
+NUM_LINE=`khal list --format "{title}" | wc -l`
+khal list --format "{title}" | sed -n ''"$CAL_LINE"'p'
+((CAL_LINE=CAL_LINE%NUM_LINE))
+((CAL_LINE++))
+#echo $CAL_LINE $NUM_LINE
+echo "CAL_LINE=$CAL_LINE" > ~/.CAL_LINE
+
+
+
